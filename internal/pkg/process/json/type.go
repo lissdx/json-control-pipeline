70818a51178031c71454be43c77c8e191232b4db
yuriliss <yuri.l@openweb.com>
2021-12-25 08:47:12 +0200

    Simple update

--- internal/pkg/process/json/type.go
+++ internal/pkg/process/json/type.go
@@ -1,8 +1,7 @@
 package json
 
-
 type addConfiguration struct {
-	FieldName string `json:"fieldName"`
+	FieldName  string `json:"fieldName"`
 	FieldValue string `json:"fieldValue"`
 }
 
@@ -21,8 +20,15 @@ type ProcessStep struct {
 
 type UnmarshalT struct {
 	Steps []struct {
-		Processor     string `json:"processor"`
+		Processor     string            `json:"processor"`
 		Configuration map[string]string `json:"configuration"`
 	} `json:"steps"`
 }
 
+//type AmpTagsRequest struct {
+//	*go_entities.BaseRequest
+//	UserIP    net.IP `json:"-"`
+//	Component string `json:"component"`
+//	Geo       string `json:"-"`
+//}
+//string `json:"-"`
